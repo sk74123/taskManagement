ab591abdfede50c39eb4a40f839be54f61efff25
Sahil Kumar <sahil400653@gmail.com>
2024-09-09 19:32:57 +0530

    Added password validation

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'city', 'country')
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'city', 'country')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = CustomUser(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
